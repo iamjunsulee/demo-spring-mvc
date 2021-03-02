e0c2801b7c29c479529ad3d7f77bf55073eb7c36
iamjunsulee <mellow0924@gmail.com>
2021-03-03 00:19:46 +0900

    form 전달 테스트

--- src/main/java/me/junsu/demospringmvc/SampleController.java
+++ src/main/java/me/junsu/demospringmvc/SampleController.java
@@ -1,8 +1,10 @@
 package me.junsu.demospringmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Controller
@@ -19,5 +21,26 @@ public class SampleController {
     @ResponseBody
     public String getParameter(@RequestParam Optional<String> name) {
         return "name : " + name.orElseGet(() -> "not provided");
+    }
+
+    @GetMapping("/event/form")
+    public String getForm(Model model) {
+        Event event = new Event();
+        event.setId(1L);
+        event.setName("hi");
+        event.setStartDateTime(LocalDateTime.now());
+        event.setEndDateTime(LocalDateTime.now());
+        event.setLimitOfEnrollment(10);
+        model.addAttribute("event", event);
+        return "events/form";
+    }
+
+    @PostMapping("/event/create")
+    @ResponseBody
+    public Event createForm(@RequestParam String name, @RequestParam int limitOfEnrollment) {
+        Event event = new Event();
+        event.setName(name);
+        event.setLimitOfEnrollment(limitOfEnrollment);
+        return event;
     }
 }
